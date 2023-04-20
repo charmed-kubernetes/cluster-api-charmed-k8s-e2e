f2fe84f6b6c8da5f850bc86b2b1169a6a3b45e36
Stone Preston <stone.preston@canonical.com>
2023-04-20 12:49:05 -0500

    install microk8s on ubuntu unit

--- tests/e2e/test_e2e.py
+++ tests/e2e/test_e2e.py
@@ -6,13 +6,22 @@ log = logging.getLogger(__name__)
 @pytest.mark.abort_on_fail
 async def test_build_and_deploy(ops_test):
     log.info("Deploying ubuntu charm")
-    await ops_test.model.deploy(
+    ubuntu_app = await ops_test.model.deploy(
         'ubuntu',
         application_name='ubuntu',
         series='jammy',
         channel='stable',
     )
     await ops_test.model.wait_for_idle()
+
+    # get the one and only unit
+    assert len(ubuntu_app.units) == 1
+    ubuntu_unit = ubuntu_app.units[0]
+
+    action = await unit.run('snap install microk8s --classic')
+    log.info(f"action results: {action.results}")
+    await ops_test.model.wait_for_idle()
+
 
 async def test_status(ops_test):
     assert ops_test.model.applications["ubuntu"].units[0].workload_status == "active"
