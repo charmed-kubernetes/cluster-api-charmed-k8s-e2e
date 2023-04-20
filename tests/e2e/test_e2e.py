2937cd1ee07d6bc6d42a22dabac6bc0edd4a3eeb
Stone Preston <stone.preston@canonical.com>
2023-04-20 15:41:17 -0500

    copy proxy config to unit

--- tests/e2e/test_e2e.py
+++ tests/e2e/test_e2e.py
@@ -33,7 +33,37 @@ async def test_build_and_deploy(ops_test):
         log.error(f"stderr:\n{stderr.strip()}")
         pytest.fail("Failed to install microk8s")
     
-    log.info(f"installation results: {stdout}")
+    log.info(f"microk8s installation results: {stdout}")
+
+    # copy proxy data to ubuntu machine
+    cmd = (
+           "juju scp "
+           "tests/data/containerd_proxy_config.txt containerd_proxy_config.txt ubuntu/0:"
+    )
+    retcode, stdout, stderr = await ops_test.run(*shlex.split(cmd))
+    if retcode != 0:
+        log.error(f"retcode: {retcode}")
+        log.error(f"stdout:\n{stdout.strip()}")
+        log.error(f"stderr:\n{stderr.strip()}")
+        pytest.fail("Failed to copy proxy data to ubuntu unit")
+
+    # list directory on unit
+    cmd = (
+           "juju exec "
+           "--unit ubuntu/0 "
+           "'ls'"
+    )
+    retcode, stdout, stderr = await ops_test.run(*shlex.split(cmd))
+    if retcode != 0:
+        log.error(f"retcode: {retcode}")
+        log.error(f"stdout:\n{stdout.strip()}")
+        log.error(f"stderr:\n{stderr.strip()}")
+        pytest.fail("Failed to list files on ubuntu unit")
+
+    log.info(f"list results: {stdout}")
+    
+    # sudo bash -c 'cat containerd_proxy_config.txt >> /var/snap/microk8s/current/args/containerd-env'
+
 
     await ops_test.model.wait_for_idle()
 
