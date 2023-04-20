ccf26f9f346725be0fe130d370b57f500c9aeda0
Stone Preston <stone.preston@canonical.com>
2023-04-20 16:51:56 -0500

    try using temp path

--- tests/e2e/test_e2e.py
+++ tests/e2e/test_e2e.py
@@ -1,14 +1,19 @@
 import logging
 import pytest
 import shlex
+import shutil
 import os
 
 log = logging.getLogger(__name__)
 
 @pytest.mark.abort_on_fail
 async def test_build_and_deploy(ops_test):
-    log.info("cwd")
-    log.info(os.getcwd())
+    log.info("data files")
+    log.info(os.listdir("tests/data"))
+    log.info("copying file")
+    shutil.copyfile("tests/data/containerd_proxy_config.txt", ops_test.tmp_path)
+    log.info("temp path")
+    log.info(os.listdir(ops_test.tmp_path))
     log.info("Deploying ubuntu charm")
     
     ubuntu_app = await ops_test.model.deploy(
@@ -40,9 +45,10 @@ async def test_build_and_deploy(ops_test):
     log.info(f"microk8s installation results: {stdout}")
 
     # copy proxy data to ubuntu machine
+    filepath = ops_test.tmp_path / "containerd_proxy_config.txt"
     cmd = (
            "juju scp "
-           "tests/data/containerd_proxy_config.txt containerd_proxy_config.txt ubuntu/0:"
+           f"{filepath} containerd_proxy_config.txt ubuntu/0:"
     )
     retcode, stdout, stderr = await ops_test.run(*shlex.split(cmd))
     if retcode != 0:
